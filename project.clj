3b4c99d4ab0b929ec6f8d811b9b76f681cbb4e3c
Martin Abelson Sahlen <sahlen@Martins-MacBook-Pro.local>
2017-02-19 23:10:16 -0500

    Add some more

--- project.clj
+++ project.clj
@@ -1,8 +1,12 @@
 (defproject hello-browser "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.456"]]
+                 [org.clojure/clojurescript "1.9.456"]
+                 [bidi "2.0.16"]]
 
-  :plugins [[lein-cljsbuild "1.1.5"]]
+  :plugins [[lein-cljsbuild "1.1.5"]
+            [lein-npm "0.6.2"]]
+
+  :npm {:dependencies [[circular-json "0.3.1"]]}
 
   :cljsbuild {
   :builds [{
--- src/core.cljs
+++ src/core.cljs
@@ -1,7 +1,27 @@
-(ns cloud-fn.core)
+(ns cloud-fn.core
+  (:require [bidi.bidi :as bidi]))
+
+(def circular-json (js/require "circular-json"))
+
+(defn parse-raw-req
+  [raw-req]
+  (js->clj (.parse js/JSON (.stringify circular-json raw-req)) :keywordize-keys true))
+
+(def my-routes ["/" [["index.html" :index]
+                    [true         :not-found]]])
+
+(defn index-handler
+  [req]
+  (:headers req))
 
 (defn greet
-  [req, res]
-  (.send res "Hello world!"))
+  [raw-req raw-res]
+  (let [req (parse-raw-req raw-req)]
+    (do
+      (println (.-path raw-req))
+      (println req)
+      (println (bidi/match-route my-routes (:path req)))
+      (.json raw-res (clj->js(:headers req))))))
 
+(enable-console-print!)
 (set! (.-exports js/module) #js {:greet greet})
